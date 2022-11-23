7fb8f76f0745ca7b54bfd146623a865c3aee71b1
AJ Schmidt <aschmidt@nvidia.com>
2022-11-23 11:02:38 -0500

    update `update-version.sh` script

--- ci/update-version.sh
+++ ci/update-version.sh
@@ -5,3 +5,5 @@ set -ue
 NEXT_VERSION=$1
 
 sed -i "/__version__/ s/\".*\"/\"${NEXT_VERSION}\"/" src/rapids_dependency_file_generator/_version.py
+
+cat src/rapids_dependency_file_generator/_version.py
