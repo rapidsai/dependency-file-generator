6e4d3fe6a8bee5f419f7d360f13b44b486853fa5
Simon Adorf <sadorf@nvidia.com>
2023-02-03 09:54:22 +0100

    Use best_match() to provide more readable error feedback.

--- src/rapids_dependency_file_generator/rapids_dependency_file_validator.py
+++ src/rapids_dependency_file_generator/rapids_dependency_file_validator.py
@@ -1,11 +1,11 @@
 """Logic for validating dependency files."""
 
 import json
-import sys
 import textwrap
 
 import jsonschema
 import pkg_resources
+from jsonschema.exceptions import best_match
 
 SCHEMA = json.loads(pkg_resources.resource_string(__name__, "schema.json"))
 
@@ -24,8 +24,9 @@ def validate_dependencies(dependencies):
         If the dependencies do not conform to the schema
     """
     validator = jsonschema.Draft7Validator(SCHEMA)
-    if not validator.is_valid(dependencies):
-        for i, error in enumerate(validator.iter_errors(dependencies), start=1):
-            print(f"Error #{i}:", file=sys.stderr)
-            print(textwrap.indent(str(error), "\t"), file=sys.stderr)
+    errors = list(validator.iter_errors(dependencies))
+    if len(errors) > 0:
+        print("The provided dependency file contains schema errors.")
+        best_matching_error = best_match(errors)
+        print("\n", textwrap.indent(str(best_matching_error), "\t"), "\n")
         raise RuntimeError("The provided dependencies data is invalid.")
